c0ee5b6ee50c9280ca7ecc729a3e4b971cfdf178
Michael Sumner <mdsumner@gmail.com>
2017-01-17 15:53:02 +1100

    very basic sf round-trip (no holes yet)

--- R/primitives_1D.r
+++ R/primitives_1D.r
@@ -1,15 +1,16 @@
+#' @importFrom utils head
 path_to_segment <- function(x, id = NULL) {
   ## this is a trick of array logic to generate paired indexes from a sequence
-  x <- stats::setNames(tibble::as_tibble(head(suppressWarnings(matrix(x, nrow = length(x) + 1, ncol = 2, byrow = FALSE)), -2L)), 
+  x <- stats::setNames(tibble::as_tibble(utils::head(suppressWarnings(matrix(x, nrow = length(x) + 1, ncol = 2, byrow = FALSE)), -2L)), 
            c(".vertex0", ".vertex1"))
   if (!is.null(id)) x[["branch_"]] <- id
   x
 }
 
 #' Given a `BRANCH`` model decompose to 1-dimensional primitives. 
-#sc_object, sc_branch, sc_coord
-#' @examples
 #' 
+#' @param x input object
+#' @param ... arguments passed to methods
 #' sf_obj <- inlandwaters[5, ]
 #' obj <- BRANCH(sf_obj)
 #' prim <- sc_primitive(obj)
@@ -24,7 +25,7 @@ sc_primitive.BRANCH <- function(x, ...) {
   ## this is a subset of RTriangle::pslg (because the original target was RTriangle::triangulate)
   #pstraight_lgraph <- list(P = as.matrix(v[, c("x_", "y_")]),
   ## only need S for 1D
-  segment_longform <- dplyr::bind_rows(lapply(all_coordinates %>% split(.$branch_),
+  segment_longform <- dplyr::bind_rows(lapply(split(all_coordinates, all_coordinates$branch_),
                                          function(x) path_to_segment(x$vertex_, x$branch_[1L])))
   
   
@@ -41,11 +42,13 @@ sc_primitive.BRANCH <- function(x, ...) {
 #' 
 #' @param x input object
 #' @param ... arguments passed to methods
+#' @importFrom dplyr select
 #' @examples
 #' 
 #' sf_obj <- inlandwaters[5, ]
 #' sf_pslg <- PRIMITIVE(sf_obj)
 #' plot(sf_pslg$vertex[, c("x_", "y_")], type = "n")
+#' library(dplyr)
 #' P1_segments <- function(x, ...) {
 #'   s1 <- x$segment %>% 
 #'       inner_join(x$vertex, c(".vertex0" = "vertex_")) %>% 
@@ -73,7 +76,44 @@ PRIMITIVE.BRANCH <- function(x, ...) {
 #' @name PRIMITIVE
 #' @export
 PRIMITIVE.sf <- function(x, ...) {
-  x <- BRANCH(x) %>% PRIMITIVE(br)
+  x <- BRANCH(x) %>% PRIMITIVE()
   class(x) <-  c("PRIMITIVE", class(x))
   x
+}
+
+#' Recompose `sf` simple features from `PRIMITIVE`` models. 
+#'
+#' @param x input object
+#' @param ... arguments passed to methods
+#'
+#' @return `PRIMITIVE`
+#' @export
+#'
+#' @examples
+#' prim <- PRIMITIVE(inlandwaters)
+#' library(sf)
+#' plot(st_as_sf(prim))
+#' @importFrom sf st_as_sf st_multipolygon st_sfc 
+#' @importFrom dplyr select inner_join
+st_as_sf.PRIMITIVE <- function(x, ...) {
+  ol <- vector("list", nrow(x$object))
+  for (i_obj in seq(nrow(x$object))) {
+   branch <- x$object[i_obj, ] %>% dplyr::select(object_) %>% 
+     inner_join(x$branch) 
+   brl <- vector("list", nrow(branch))
+    for (i_br in seq(nrow(branch))) {
+      br_0 <- branch[i_br, ] %>% 
+     inner_join(x$branch_link_vertex) %>% 
+     inner_join(x$vertex) %>% 
+     split(.$island_)
+      ## not getting holes properly
+     brl[[i_br]] <- lapply(br_0, function(aa) as.matrix(aa[c(seq_len(nrow(aa)), 1L), c("x_", "y_")]))
+    }
+   ## slow, need to class all the structure without going through sf checks
+   ol[[i_obj]] <- sf::st_multipolygon(brl)
+  }
+  ## TODO: need round-trip crs
+  sfd <- as.data.frame(x$object)
+  sfd[["geometry"]] <- sf::st_sfc(ol)
+  sf::st_as_sf(sfd)
 }
--- tests/testthat/test-primitives.R
+++ tests/testthat/test-primitives.R
@@ -1,12 +1,14 @@
 context("primitives")
 
-# 
+#
 # test_that("primitives 0D", {
 #   error("no tests!")
 # })
-# test_that("primitives 1D", {
-#   error("no tests!")
-# })
+test_that("primitives 1D", {
+   inlandwaters[5, ] %>% PRIMITIVE() %>% expect_s3_class("PRIMITIVE") %>% 
+   ## test round-trip back to sf
+   st_as_sf() %>% expect_s3_class("sf")
+})
 # 
 # test_that("primitives 2D", {
 #   error("no tests!")
