cf467b62e292769653c646e3075dbfac5af6651e
Michael Sumner <mdsumner@gmail.com>
2017-09-18 11:49:34 +1000

    bulid_sf internal, now fast enough

--- R/sf-builder.R
+++ R/sf-builder.R
@@ -1,7 +1,10 @@
 #' a pattern for building sf objects from 
 #' - a gibble, the map of the paths
 #' - the coordinates
-#' the map is an encoding of the structural 
+#' the map is an encoding of the structural properties of the geometry
+#' 
+#' currently only XY is supported
+#' @noRd
 build_sf <- function(gm, coords_in, crs = NULL) {
   glist <- vector("list", length(unique(gm$object)))
   coords_in <- gm %>% dplyr::select(-type, -ncol, -nrow) %>%
@@ -21,25 +24,33 @@ build_sf <- function(gm, coords_in, crs = NULL) {
     ## object becomes sub-feature element (not a hole, that is "part")
     coord0$path <- rep(seq_len(nrow(gm0)), gm0$nrow)
     ## todo need to set XY, XYZ, XYZM, XYM
+    cnames <- c("x_", "y_")
+    pathnames <- c(cnames, "path")
+
     feature <- switch(type,
-                                POINT = structure(unlist(coord0 %>% dplyr::select(x_, y_)), class = c("XY", "POINT", "sfg")),
-                                MULTIPOINT = structure(as.matrix(coord0 %>% dplyr::select(x_, y_)), class = c("XY", "MULTIPOINT", "sfg")),
-                                LINESTRING = structure(as.matrix(coord0 %>% dplyr::select(x_, y_)), class = c("XY", "LINESTRING", "sfg")),
-                                MULTILINESTRING = structure(lapply(split(coord0 %>% dplyr::select(x_, y_), coord0$path), as.matrix), class = c("XY", "MULTILINESTRING", "sfg")),
-                                POLYGON = structure(split_to_matrix(coord0 %>% dplyr::select(x_, y_), coord0$path), class = c("XY", "POLYGON", "sfg")),
-                                MULTIPOLYGON = structure(lapply(split(coord0 %>% dplyr::select(x_, y_, path), coord0$subobject),
-                                                                          function(path) split_to_matrix(path %>% select(x_, y_), path$path)), 
+                                POINT = structure(unlist(coord0[cnames]), class = c("XY", "POINT", "sfg")),
+                                MULTIPOINT = structure(as.matrix(coord0[cnames]), class = c("XY", "MULTIPOINT", "sfg")),
+                                LINESTRING = structure(as.matrix(coord0[cnames]), class = c("XY", "LINESTRING", "sfg")),
+                                MULTILINESTRING = structure(lapply(split(coord0[cnames], coord0[["path"]]), as.matrix), class = c("XY", "MULTILINESTRING", "sfg")),
+                                POLYGON = structure(split_to_matrix(coord0[cnames], coord0[["path"]]), class = c("XY", "POLYGON", "sfg")),
+                                MULTIPOLYGON = structure(lapply(split(coord0[pathnames], coord0[["subobject"]]),
+                                                                          function(path) split_to_matrix(path[cnames], path[["path"]])), 
                                                          class = c("XY", "MULTIPOLYGON", "sfg"))
     )
-    
+
     glist[[ifeature]] <- feature
   }
-  if (is.null(crs)) crs <- NA_crs_
-  #sfd[["geometry"]] <- structure(ol, class = c("sfc_MULTIPOLYGON", "sfc"  ), n_empty = 0, precision = 0, crs = na_crs, bbox = bb)
-  #structure(sfd, sf_column = "geometry", agr = factor(NA, c("constant", "aggregate", "identity")), class = c("sf", class(sfd)))
-  
-  out <-   st_sfc(glist, crs = crs)
-  out
+  if (is.null(crs)) {
+    crs <- structure(list(epsg = NA_real_, proj4string = NA_character_, class = "crs"))
+  } else {
+    crs <- switch(mode(crs), 
+        character = structure(list(epsg = NA_real_, proj4string = crs, class = "crs")), 
+        numeric = structure(list(epsg = crs, proj4string = NA_character_, class = "crs")))
+  }
+  bb <- c(range(coords_in[["x_"]]), range(coords_in[["y_"]]))[c(1, 3, 2, 4)]
+  names(bb) <- structure(c("xmin", "ymin", "xmax", "ymax"), class = "bbox")
+  glist <- structure(glist, class = c(sprintf("sfc_%s", type), "sfc"  ), n_empty = 0, precision = 0, crs = crs, bbox = bb)
+ glist
   
 }
 
