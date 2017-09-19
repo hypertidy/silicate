dd63d02bfa5e3cb73d19127276ce419937f528fb
Michael Sumner <mdsumner@gmail.com>
2017-09-19 15:23:40 +1000

    early OSM

---
+++ inst/examples/osmdata/readosm.R
@@ -0,0 +1,90 @@
+# library(osmdata)
+# library(xml2)
+# gda94 <- matrix(rep(c(521494, 5247625), each = 2) + c(-1, 1) * 100, ncol = 2)
+# ext <- rgdal::project(gda94, "+init=epsg:28355", inv = TRUE)
+# 
+# q <- opq(bbox = as.vector(t(ext)))
+# #q <- add_osm_feature(q0, key = 'name', value = "Thames", value_exact = FALSE)
+# 
+# encoding <- "UTF-8"
+# obj <- osmdata()
+# obj$bbox <- q$bbox
+# obj$overpass_call <- opq_string(q)
+# doc <- osmdata:::overpass_query(query = obj$overpass_call, quiet = FALSE,
+#                                 encoding = encoding)
+# 
+# #saveRDS(doc, file = "inst/examples/osmdata/doc.rds")
+# x <- readRDS("inst/examples/osmdata/doc.rds")
+#xml2::xml_find_first(xml2::read_xml(doc), "//way")
+
+library(dplyr)
+read_node <- function(x, ...) {
+  lst <- xml2::xml_find_all(xml2::read_xml(x), "//node") %>% 
+    purrr::map(xml_attrs)
+  tibble::as_tibble(do.call(rbind, lst)) %>% 
+    dplyr::transmute(vertex_ = id, x_ = as.numeric(lon), y_ = as.numeric(lat))
+}
+read_way <- function(x, ...) {
+  do.call(rbind, xml2::xml_find_all(xml2::read_xml(x), "//way") %>% 
+            purrr::map(xml_attrs)) %>% 
+    tibble::as_tibble() %>% 
+    dplyr::rename(path = id)  %>% mutate(object = rev_string(path))
+  
+}
+read_way_ref <- function(x, ...) {
+  path <- read_way(x)
+#  path <- dplyr::mutate(path, id_ = as.character(seq_len(nrow(path))))
+  xml_find_all(xml2::read_xml(x), "//way") %>% 
+    map(xml_find_all, "nd") %>% 
+    map(function(a) do.call(rbind, xml_attrs(a)) %>% tibble::as_tibble() %>% dplyr::rename(vertex_ = ref)) %>% 
+    setNames(path[["path"]]) %>% 
+  bind_rows(.id = "path")  
+}
+rev_string <- function(x) {
+  unlist(lapply(strsplit(x, ""), function(y) paste(rev(y), collapse = "")))
+}
+read_obj <- function(x) {
+  read_way(x) %>% dplyr::transmute(object = rev_string(path)) 
+}
+
+library(purrr)
+library(xml2)
+
+silicate_osm <- function(x) {
+  structure( list(object =   read_obj(x),
+       path =   read_way(x),
+       path_link_vertex = read_way_ref(x), 
+       vertex = read_node(x)),
+      join_ramp = c("object",           "path",             "path_link_vertex", "vertex"), 
+      class = c("PATH", "sc"))
+}
+
+path_from_osm <- silicate_osm(doc)
+dim(path_from_osm$vertex)
+## the silicate generics
+## note how sc_coord *expands* to the full set of instances
+sc_coord(path_from_osm)
+sc_path(path_from_osm)
+sc_object(path_from_osm)
+library(gibble)
+gibble.PATH <- function(x, ...) {
+  inner_join(x[["path"]], x[["path_link_vertex"]] %>% group_by(path) %>% summarize(nrow = n()) ) %>% 
+    dplyr::mutate(ncol = 2, type = "MULTILINESTRING")
+}
+gm <- gibble(path_from_osm)
+library(sf)
+plot(st_sf(geometry = silicate:::build_sf(gm, sc_coord(path_from_osm)), a = unique(gm$object)))
+# library(osmdata)
+# gda94 <- matrix(rep(c(521494, 5247625), each = 2) + c(-1, 1) * 100, ncol = 2)
+# ext <- rgdal::project(gda94, "+init=epsg:28355", inv = TRUE)
+# 
+# q <- opq(bbox = as.vector(t(ext)))
+# #q <- add_osm_feature(q0, key = 'name', value = "Thames", value_exact = FALSE)
+# 
+# encoding <- "UTF-8"
+# obj <- osmdata()
+# obj$bbox <- q$bbox
+# obj$overpass_call <- opq_string(q)
+# doc <- osmdata:::overpass_query(query = obj$overpass_call, quiet = FALSE, 
+#                                 encoding = encoding)
+# #doc
