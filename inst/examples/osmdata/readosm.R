e5499ddf4dcd12c3b4240883e3a20f921ab19bd2
Michael Sumner <mdsumner@gmail.com>
2017-09-19 20:38:47 +1000

    osm example

--- inst/examples/osmdata/readosm.R
+++ inst/examples/osmdata/readosm.R
@@ -1,21 +1,27 @@
-# library(osmdata)
-# library(xml2)
-# gda94 <- matrix(rep(c(521494, 5247625), each = 2) + c(-1, 1) * 100, ncol = 2)
-# ext <- rgdal::project(gda94, "+init=epsg:28355", inv = TRUE)
-# 
-# q <- opq(bbox = as.vector(t(ext)))
-# #q <- add_osm_feature(q0, key = 'name', value = "Thames", value_exact = FALSE)
-# 
-# encoding <- "UTF-8"
-# obj <- osmdata()
-# obj$bbox <- q$bbox
-# obj$overpass_call <- opq_string(q)
-# doc <- osmdata:::overpass_query(query = obj$overpass_call, quiet = FALSE,
-#                                 encoding = encoding)
-# 
+#############################################################
+## get some OSM
+library(osmdata)
+library(xml2)
+gda94 <- matrix(rep(c(521494, 5247625), each = 2) + c(-1, 1) * 100, ncol = 2)
+ext <- rgdal::project(gda94, "+init=epsg:28355", inv = TRUE)
+
+q <- opq(bbox = as.vector(t(ext)))
+#q <- add_osm_feature(q0, key = 'name', value = "Thames", value_exact = FALSE)
+
+encoding <- "UTF-8"
+obj <- osmdata()
+obj$bbox <- q$bbox
+obj$overpass_call <- opq_string(q)
+doc <- osmdata:::overpass_query(query = obj$overpass_call, quiet = FALSE,
+                                encoding = encoding)
+
 # #saveRDS(doc, file = "inst/examples/osmdata/doc.rds")
 # x <- readRDS("inst/examples/osmdata/doc.rds")
-#xml2::xml_find_first(xml2::read_xml(doc), "//way")
+
+#############################################################
+## functions to silicify the OSM message
+## after parsing out ways (path), nodes (vertex) and
+##  pretending (for now) that each way is a feature (object)
 
 library(dplyr)
 read_node <- function(x, ...) {
@@ -47,8 +53,10 @@ read_obj <- function(x) {
   read_way(x) %>% dplyr::transmute(object = rev_string(path)) 
 }
 
-library(purrr)
-library(xml2)
+gibble.PATH <- function(x, ...) {
+  inner_join(x[["path"]], x[["path_link_vertex"]] %>% group_by(path) %>% summarize(nrow = n()) ) %>% 
+    dplyr::mutate(ncol = 2, type = "MULTILINESTRING")
+}
 
 silicate_osm <- function(x) {
   structure( list(object =   read_obj(x),
@@ -59,32 +67,43 @@ silicate_osm <- function(x) {
       class = c("PATH", "sc"))
 }
 
+
+#############################################################
+## bring in to silicate PATH form
+library(gibble)
+library(purrr)
+library(xml2)
+library(silicate)
 path_from_osm <- silicate_osm(doc)
 dim(path_from_osm$vertex)
-## the silicate generics
+## the silicate generics now work, because PATH is a 
+## common-form
+
 ## note how sc_coord *expands* to the full set of instances
-sc_coord(path_from_osm)
-sc_path(path_from_osm)
-sc_object(path_from_osm)
-library(gibble)
-gibble.PATH <- function(x, ...) {
-  inner_join(x[["path"]], x[["path_link_vertex"]] %>% group_by(path) %>% summarize(nrow = n()) ) %>% 
-    dplyr::mutate(ncol = 2, type = "MULTILINESTRING")
-}
+#sc_coord(path_from_osm)
+#sc_path(path_from_osm)
+#sc_object(path_from_osm)
+
+## gibble is the geometry map, a run-length code of 
+## the expanded coordinate set
 gm <- gibble(path_from_osm)
 library(sf)
-plot(st_sf(geometry = silicate:::build_sf(gm, sc_coord(path_from_osm)), a = unique(gm$object)))
-# library(osmdata)
-# gda94 <- matrix(rep(c(521494, 5247625), each = 2) + c(-1, 1) * 100, ncol = 2)
-# ext <- rgdal::project(gda94, "+init=epsg:28355", inv = TRUE)
-# 
-# q <- opq(bbox = as.vector(t(ext)))
-# #q <- add_osm_feature(q0, key = 'name', value = "Thames", value_exact = FALSE)
-# 
-# encoding <- "UTF-8"
-# obj <- osmdata()
-# obj$bbox <- q$bbox
-# obj$overpass_call <- opq_string(q)
-# doc <- osmdata:::overpass_query(query = obj$overpass_call, quiet = FALSE, 
-#                                 encoding = encoding)
-# #doc
+sf_geom <- silicate:::build_sf(gm, sc_coord(path_from_osm))
+## fakey up a full sf-feature data frame
+sf_obj <- st_set_crs(st_sf(geometry = sf_geom, a = unique(gm$object)), 4326)
+
+g <- dismo::gmap(as(sf_obj, "Spatial"), type = "satellite")
+library(raster)
+plot(g, addfun = function() plot(st_transform(sf_obj, projection(g)), add = T))
+
+###################################################
+## convert to PRIMITIVE and provide a summary
+prim <- path_from_osm %>% PRIMITIVE()
+## no. of segments
+nrow(prim$segment)
+## no. of unique vertices
+nrow(prim$vertex)
+## no. of segments per way
+prim$segment %>% group_by(path) %>% tally()
+## no. of nodes per way
+prim$path_link_vertex %>% group_by(path) %>% tally()
