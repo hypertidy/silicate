ed8cc0a4b90502e8a1cdbaaa315a1faaea97cde5
Michael Sumner <mdsumner@gmail.com>
2017-12-21 15:24:43 +1100

    sp tests

---
+++ tests/testthat/test-sp-decomposition.R
@@ -0,0 +1,19 @@
+context("test-sp-decomposition.R")
+
+library(sf)
+library(gibble)
+spobj <- as(minimal_mesh, "Spatial")
+test_that("sp decomposition works", {
+  gibble(spobj) %>% expect_s3_class("tbl_df")
+  sc_coord(spobj) %>% expect_named(c("x_", "y_"))
+  sc_object(spobj) %>% expect_named(names(spobj))
+  p <- sc_path(spobj)
+  expect_false(any(is.na(as.matrix(p))))
+  
+  a <- sc_arc(spobj)
+  a %>% expect_named(c("arc_", "vertex_"))
+  pp <- PATH(spobj)
+  expect_equal(unname(unlist(lapply(pp, dim))), c(2L, 2L, 3L, 7L, 14L, 3L, 19L, 2L))
+  e <- sc_edge(spobj)
+  e %>% expect_named(c(".vertex0", ".vertex1", "edge_"))
+})
