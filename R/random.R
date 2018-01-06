e2c1ab09b28af5bc6354ae77f65fd541d32d2659
Michael Sumner <mdsumner@gmail.com>
2018-01-06 12:52:51 +1100

    basics of new sc_uid

--- R/random.R
+++ R/random.R
@@ -1,10 +1,40 @@
 
-#' Random IDs. 
+#' Unique labels
 #' 
-#' See `ids` package. 
+#' Find unique labels for entities, or create them
+#' if not present. 
+#' 
+#' See `ids` package for `random_id` when they are created. 
 #' @param n number of bytes per ID
 #' @export
-#' @importFrom ids random_id
-sc_uid <- function(n = 1L) {
-  ids::random_id(n, bytes = 4)
+sc_uid <- function(x, ...) {
+  UseMethod("sc_uid")
 }
+#' @export
+#' @importFrom ids random_id
+sc_uid.default <- function(x, ...) {
+  if (is.numeric(x)) {
+    warning("old behaviour where input is integer to be removed")
+  }
+  ids::random_id(x, bytes = 4L)
+}
+#' @export
+sc_uid.numeric <- function(x, ...) {
+  ids::random_id(x, bytes = 4L)
+}
+#' @export
+sc_uid.data.frame <- function(x, ...) {
+  rn <- row.names(x)
+  nr <- dim(x)[1L]
+  if (identical(rn, as.character(seq_len(nr)))) {
+    rn <- sc_uid(nr)
+  }
+  rn
+}
+sc_uid.Spatial <- function(x, ...) {
+  nr <- if (spbabel:::has_data(x)) dim(x)[1L] else length(x)
+  sc_uid(nr)
+}
+  
+  
+  
