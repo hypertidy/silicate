22cfc9f9e723e5273840d37bceb8e1a6cd192045
Michael Sumner <mdsumner@gmail.com>
2018-02-14 23:07:33 +1100

    track gibble fixes for sp, update docs

--- tests/testthat/test-sp-decomposition.R
+++ tests/testthat/test-sp-decomposition.R
@@ -3,11 +3,12 @@ context("test-sp-decomposition.R")
 library(sf)
 library(gibble)
 spobj <- as(minimal_mesh, "Spatial")
+splineobj <- as(spobj, "SpatialLinesDataFrame")
 test_that("sp decomposition works", {
   gibble(spobj) %>% expect_s3_class("tbl_df")
   sc_coord(spobj) %>% expect_named(c("x_", "y_"))
   sc_object(spobj) %>% expect_named(names(spobj))
-  p <- sc_path(spobj)
+  p <- sc_path(spobj) 
   expect_false(any(is.na(as.matrix(p))))
   
   a <- sc_arc(spobj)
@@ -19,4 +20,15 @@ test_that("sp decomposition works", {
   expect_equal(unname(unlist(lapply(pp, dim))), c(2L, 2L, 3L, 7L, 19L, 2L, 14L, 3L))
   e <- sc_edge(spobj)
   e %>% expect_named(c(".vertex0", ".vertex1", "edge_"))
+  
+  sc_coord(splineobj) %>% expect_s3_class("tbl_df") %>% nrow() %>% expect_equal( 19L)
+  sc_object(splineobj)%>% expect_s3_class("tbl_df") %>% nrow() %>% expect_equal( 2L)
+  sc_path(splineobj) %>% expect_s3_class("tbl_df") %>% 
+    nrow() %>% 
+    expect_equal( 3L)
+  sc_node(splineobj) %>% expect_s3_class("tbl_df") 
+  sc_arc(splineobj) %>% expect_s3_class("tbl_df") 
+  sc_vertex(splineobj) %>% expect_s3_class("tbl_df") 
+  sc_node(splineobj) %>% expect_s3_class("tbl_df") 
+  
 })
