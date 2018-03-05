0fae39f0acae2996c5ecace4c494582d340bbf30
Michael Sumner <mdsumner@gmail.com>
2018-03-06 06:44:15 +1100

    unfinished SC.TRI

--- R/SC-model.R
+++ R/SC-model.R
@@ -87,6 +87,35 @@ SC.default <- function(x, ...) {
             class = c("SC", "sc"))
 }
 
+
+
+## need to identify segments that were input and are
+## shared by two triangles, set to invisible
+tri_to_seg <- function(x) {
+  x[c(1, 2, 3, 2, 3, 1)]
+}
+
+to_tibble <- function(x) {
+  setNames(tibble::as_tibble(matrix(x, ncol = 2)), c(".vertex0", ".vertex1"))
+}
+## triangle classification
+SC.TRI <- function(x, ...) {
+  segment <- purrr::map_df(purrr::transpose(x$triangle[c(".vertex0", ".vertex1", ".vertex2")]), 
+                           ~to_tibble(tri_to_seg(unlist(.x))), .id = "triangle_")
+  segment$triangle_ <- x$triangle$triangle_[as.integer(segment$triangle_)]
+  segment$segment_ <- silicate::sc_uid(nrow(segment))
+  edges <- as.integer(factor(apply(cbind(segment$.vertex0, segment$.vertex1), 1, 
+                                   function(x) paste(sort(x), collapse = "-"))))
+  segment$edge_ <- sc_uid(length(unique(edges)))[edges]
+  #object_link_edge <- tibble(object_ = x$object_link_triangle$object_[match(edge$triangle_, x$object_link_triangle$triangle_)],  
+  #                           segment_ = segment$segment_)
+ # structure(list(object = x$object, object_link_edge = object_link_edge, 
+#                 edge = edge, vertex = x$vertex), class = c("SC", "sc"))
+1  ## unfinished
+  }
+
+
+
 compact_labels <- function(x) {
   v0 <- as.integer(match(x$edge$.vertex0, x$vertex$vertex_))
   v1 <- as.integer(match(x$edge$.vertex1, x$vertex$vertex_))
