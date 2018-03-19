cc6b0ccf8e166f6e852ea6ef938254ddaa7bad16
Michael Sumner <mdsumner@gmail.com>
2018-03-19 17:26:07 +1100

    add obvious sc_start and sc_end helpers for sc_edge/sc_segment

--- R/00_edge.R
+++ R/00_edge.R
@@ -4,9 +4,15 @@
 #' 
 #' Edges are unique, undirected line segments. Compare to `sc_segment` which refers to all
 #' instances of edges. 
+#' 
+#' `sc_start` and `sc_end` are convenience functions that provide the obvious
+#' start and end coordinates by joining on the appropriate edge vertex label, `.vertex0` 
+#' or `.vertex1`. Currently this returns the ordered segments, along with their unique (unordered) `edge_`, as
+#' well as unique `segment`, and `object_` labels. 
 #' @param x input object
 #' @param ... arguments for methods
 #' @name sc_edge
+#' @aliases sc_start sc_end
 #' @export
 sc_edge <- function(x, ...) {
   UseMethod("sc_edge")
@@ -26,4 +32,58 @@ sc_edge.SC <- function(x, ...) {
 sc_edge.PATH <- function(x, ...) {
   sc_segment(x, ...) %>% dplyr::distinct(.data$edge_, .keep_all = TRUE) %>% 
     dplyr::select(.data$.vertex0, .data$.vertex1, .data$edge_)
+}
+
+#' @name sc_edge
+#' @export
+sc_start <- function(x, ...) {
+  UseMethod("sc_start")
+}
+#' @name sc_edge
+#' @export
+sc_start.SC <- function(x, ...) {
+  sc_segment(x, ...) %>% dplyr::inner_join(sc_vertex(x), c(".vertex0" = "vertex_")) %>% 
+    dplyr::select(.data$x_, y_, .data$segment_, .data$edge_, .data$object_)
+}
+#' @name sc_edge
+#' @export
+sc_start.PATH <- function(x, ...) {
+  sc_start(SC(x), ...)
+}
+#' @name sc_edge
+#' @export
+sc_start.ARC <- function(x, ...) {
+  sc_start(SC(x), ...)
+}
+#' @name sc_edge
+#' @export
+sc_start.TRI <- function(x, ...) {
+  sc_start(SC(x), ...)
+}
+
+#' @name sc_edge
+#' @export
+sc_end <- function(x, ...) {
+  UseMethod("sc_end")
+}
+#' @name sc_edge
+#' @export
+sc_end.SC <- function(x, ...) {
+  sc_segment(x, ...) %>% dplyr::inner_join(sc_vertex(x), c(".vertex1" = "vertex_")) %>% 
+  dplyr::select(.data$x_, y_, .data$segment_, .data$edge_, .data$object_)
+}
+#' @name sc_edge
+#' @export
+sc_end.PATH <- function(x, ...) {
+  sc_end(SC(x), ...)
+}
+#' @name sc_edge
+#' @export
+sc_end.ARC <- function(x, ...) {
+  sc_end(SC(x), ...)
+}
+#' @name sc_edge
+#' @export
+sc_end.TRI <- function(x, ...) {
+  sc_end(SC(x), ...)
 }
