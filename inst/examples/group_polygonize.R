ee8c585667c9535fbcc4c60800c25b98bb2d3eba
Michael Sumner <mdsumner@gmail.com>
2018-04-01 09:55:16 +1000

    a more sf approach

--- inst/examples/group_polygonize.R
+++ inst/examples/group_polygonize.R
@@ -1,9 +1,9 @@
 library(raster)
 m <- matrix(1:12, 3, 4) %/% 4
 m <- volcano %/% 20
-
+library(dplyr)
 r <- setExtent(raster(m), extent(0, nrow(m), 0, ncol(m)))
-#r <- anglr::gebco1 %/% 1000
+r <- anglr::gebco1 %/% 1000
 #plot(r)
 
 library(anglr)
@@ -29,34 +29,29 @@ f <- function(x) head(matrix(x,  ncol = 2, nrow = length(x) + 1), -1L)
 
 #c1 <- function(x) c(x, x[1L])
 prj <- projection(r)
-ps <- RTriangle::pslg(P = as.matrix(uvertex[c("x_", "y_")]))
-
+V <- as.matrix(uvertex[c("x_", "y_")])
+ps <- RTriangle::pslg(P = V)
+library(sf)
 for (igroup in seq_along(uvalues)) {
   x0 <- dplyr::filter(qXv, value == uvalues[igroup])
   edges <- do.call(rbind, lapply(split(x0$vertex_, x0$quad_), f))
+  edges <- cbind(pmin(edges[,1], edges[,2]), pmax(edges[,1], edges[,2]))
+  #edges <- edges[!duplicated(edges), ]
+
   ## detect all edges that aren't on a boundary, they only occur once
-  ## no need to sort as all edges are in the same order, so identity is fine
-  #edges <- t(apply(edges, 1, sort))
   edges <- edges %>% tibble::as_tibble() %>%
     setNames(c(".vx0", ".vx1")) %>% dplyr::mutate(edgelab = paste(.vx0, .vx1, sep = "-")) %>%
     dplyr::group_by(edgelab) %>% dplyr::mutate(count = n()) %>% dplyr::ungroup() %>%
-    dplyr::filter(count < 2)
+    dplyr::filter(count < 2) %>% arrange(.vx0, .vx1)
 
-ps$S <- as.matrix(edges[c(".vx0", ".vx1")])
-  tr <- RTriangle::triangulate(ps)
-  TT <- split(t(cbind(tr$T, tr$T[,1])), rep(seq_len(nrow(tr$T)), each = 4L))
- bb <- c(range(ps$P[c(tr$T),1]), range(ps$P[c(tr$T),2]))[c(1, 3, 2, 4)]
+l <- purrr::transpose(edges[c(".vx0", ".vx1")]) %>%
+  purrr::map(~st_linestring(V[unlist(.x), ])) %>% st_sfc()
 
-  names(bb) <- c("xmin", "ymin", "xmax", "ymax")
-bb <- structure(bb, crs = NA_crs_, class = "bbox")
-  #list_sf[[igroup]] <- sf::st_union(sf::st_sfc(apply(tr$T, 1, function(i) fa_st_polygon(list(ps$P[c(i, i[1]), ]))), crs = projection(r)))
-  list_sf[[igroup]] <- sf::st_union(fa_st_sfc(lapply(TT,
-      function(i) fa_st_polygon(list(ps$P[i, ]))), crs = NA_crs_, bbox = bb))
-
+list_sf[[igroup]] <- st_polygonize(st_union(l))
   print(igroup)
 }
 
 
-x <- sf::st_sf(value = uvalues, geometry = sf::st_cast(do.call(c, list_sf)))
+x <- sf::st_sf(value = uvalues, geometry = do.call(c, list_sf)) %>% group_by(value) %>% summarize() %>% st_cast("MULTIPOLYGON")
 
 plot(x)
