7b7bfa019cbc821af27902b1347453a9ffaa5297
Michael Sumner <mdsumner@gmail.com>
2018-04-02 09:38:35 +1000

    experimental raw-interpretation

---
+++ R/silicate.R
@@ -0,0 +1,58 @@
+##
+include_nn <- function(x, y, name) {
+  if (!is.null(y)) x[[name]] <- tibble::as_tibble(y)
+  x
+}
+has_thing <- function(x, thing) {
+  thing %in% names(x) && tibble::is_tibble(x[[thing]])
+}
+has_vertex <- function(x) {
+  has_thing(x, "vertex")
+}
+
+has_edge <- function(x) {
+ has_thing(x, "edge")
+}
+has_path <- function(x) {
+  has_thing(x, "path")
+}
+has_object <- function(x) {
+  has_thing(x, "object")
+}
+
+silicate <- function(vertex = NULL, edge = NULL, path = NULL, object = NULL, model = NULL) {
+  x <- model %||% list()
+  x <- include_nn(x, vertex, "vertex")
+  x <- include_nn(x, edge, "edge")
+  x <- include_nn(x, path, "path")
+  x <- include_nn(x, object, "object")
+
+  if (length(x) == 0L) warning("empty silicate shell")
+  structure(x, class = "sc")
+}
+
+## has vertex
+## has vertex + edges (edge-triangulation, drop path)
+## has vertex + path (path-triangulation, no edges)
+## has object (I think we must ignore unless linked by edges or path)
+TRI.sc <- function(x, ...) {
+  if (has_vertex(x)) {
+    if (has_edge(x)) {
+      stop("constrained triangulation not supported, use anglr::DEL or remove edges")
+    } else {
+      d <- x[["vertex"]]
+    }
+  } else {
+    stop("no vertices to triangulate")
+
+  }
+  quo_named <- rlang::quos(...)
+  if (length(quo_named) < 1) {
+    nms <- names(d)
+    quo_named <- rlang::syms(nms)
+    message(sprintf("no columns specified, using %s", paste(nms, collapse = ", ")))
+  }
+   out <- try(geometry::delaunayn(as.matrix(dplyr::select(d, !!!quo_named))), silent = TRUE)
+
+  if (inherits(out, "try-error")) stop("triangulation failed") else out
+}
