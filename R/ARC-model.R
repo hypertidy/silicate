603f64c1f54b07eb735a5feb6bc9f276f8764fd3
Michael Sumner <mdsumner@gmail.com>
2018-10-29 11:24:13 +0000

    arc

--- R/ARC-model.R
+++ R/ARC-model.R
@@ -1,18 +1,18 @@
 
-  
+
 #' ARC model
 #'
-#' Arcs are paths within the line segment graph between nodes. 
-#' 
-#' Nodes are the vertices where three or more arcs meet. 
-#' An arc can exist without including any nodes, a path that has no neighbouring relationship with another path. 
-#' 
-#' This is _not_ the same terminology as used by other systems. The `arc_link_vertex` mapping is inherently ordered, 
-#' but we don't consider order of arcs. Duplicated arcs (i.e. complementary turns around neighbouring polygons) are 
-#' not kept. The `object_link_arc` mapping records which arc belongs to the objects, so can be reconstructed within 
+#' Arcs are paths within the line segment graph between nodes.
+#'
+#' Nodes are the vertices where three or more arcs meet.
+#' An arc can exist without including any nodes, a path that has no neighbouring relationship with another path.
+#'
+#' This is _not_ the same terminology as used by other systems. The `arc_link_vertex` mapping is inherently ordered,
+#' but we don't consider order of arcs. Duplicated arcs (i.e. complementary turns around neighbouring polygons) are
+#' not kept. The `object_link_arc` mapping records which arc belongs to the objects, so can be reconstructed within
 #' objects by tracing `arc_link_vertex` start and end point identity.
 #' @inheritParams SC
-#' 
+#'
 #' @return ARC model
 #' @export
 #'
@@ -28,6 +28,7 @@ ARC <- function(x, ...) {
 ARC.default <- function(x, ...) {
   ARC(PATH(x), ...)
 }
+
 #' @name ARC
 #' @export
 ARC.PATH <- function(x, ...) {
@@ -39,30 +40,30 @@ ARC.PATH <- function(x, ...) {
   v <- sc_vertex(x)
   #join_ramp <-  tabnames <- c("object", "path",  "path_link_vertex", "vertex")
   meta <- tibble(proj = get_projection(x), ctime = format(Sys.time(), tz = "UTC"))
-  
-  lst <- list(object = o, 
-              object_link_arc = oXa, 
-          #    arc = arc, 
-              arc_link_vertex = aXv, 
+
+  lst <- list(object = o,
+              object_link_arc = oXa,
+          #    arc = arc,
+              arc_link_vertex = aXv,
               vertex = v, meta = meta
   )
   structure(lst, class = c("ARC", "sc"), join_ramp = c("object", "object_link_arc", "arc_link_vertex", "vertex"))
 }
 PATH.ARC <- function(x, ...) {
   o <- sc_object(x)
-  paths <- dplyr::inner_join(o[1, "object_"], x$object_link_arc) %>% 
+  paths <- dplyr::inner_join(o[1, "object_"], x$object_link_arc) %>%
     dplyr::inner_join(x$arc_link_vertex)
 }
 unique_arcs <- function(x, ...) {
   dat <- split(x, x$arc_)
-  arc_id <- dat %>% 
+  arc_id <- dat %>%
     purrr::map(function(.x) paste(first_sort(.x$vertex_), collapse = ""))
   bind_rows(dat[!duplicated(arc_id)])
 }
 
 first_sort <- function(x) {
   if (x[1L] > x[length(x)]) {
-    rev(x) 
+    rev(x)
   } else {
     x
   }
