7c7eb381a8fae70cefda5459730e26db083b3189
Michael Sumner <mdsumner@gmail.com>
2020-03-02 02:24:26 +0000

    cleanup dispatch

--- R/get-projection.R
+++ R/get-projection.R
@@ -10,10 +10,21 @@ get_projection.BasicRaster <- function(x, ...) {
   x@crs
 }
 get_projection.sf <- function(x, ...) {
-  attr(x[[attr(x, "sf_column")]], "crs")[["proj4string"]]
+  get_projection(x[[attr(x, "sf_column")]]) 
 }
 get_projection.sfc <- function(x, ...) {
-  attr(x, "crs")[["proj4string"]]
+  crs <- attr(x, "crs")
+  if (!is.null(crs)) {
+   if ("proj4string" %in% names(crs)) {
+     out <- unclass(crs)[["proj4string"]]
+   } else {
+     out <- "NA_character_"
+   }
+  }
+  if (is.na(out)) {
+    warning("cannot get proj string from sf object")
+  }
+  out
 }
 get_projection.sc <- function(x, ...) {
   x$meta$proj[1L]
