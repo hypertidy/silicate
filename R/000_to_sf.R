4b7c01a4b5f9819d6a351a721fc40834cf534ce4
Michael Sumner <mdsumner@gmail.com>
2020-04-02 10:51:46 +1100

    sf build for ARC, PATH/0

--- R/000_to_sf.R
+++ R/000_to_sf.R
@@ -14,14 +14,15 @@
 #' to_sf(PATH(minimal_mesh))
 #' to_sf(PATH0(minimal_mesh))
 #'
-#' #to_sf(ARC(minimal_mesh))
+#' to_sf(ARC(minimal_mesh))
 to_sf.sc <- function(x, sfc_type = NULL, geom_type = NULL) {
   df <- sc_coord(x)
   names(df) <- gsub("_", "", names(df))
   path <- sc_path(x)
   ncoord <- path[["ncoords_"]]
 
-  sfc_type <- unique(path$type)
+  sfc_type <- unique(path[["type"]]) %||% "LINESTRING"
+  if (grepl("^MULTI", sfc_type)) sfc_type <- "MULTILINESTRING"
   stopifnot(length(sfc_type) == 1L)
   if (sfc_type == "LINESTRING") {
     # df[["multilinestring_id"]] <-
@@ -32,7 +33,15 @@ to_sf.sc <- function(x, sfc_type = NULL, geom_type = NULL) {
                           #.data$multilinestring_id,
                               .data$linestring_id)
   }
-
+  if (sfc_type == "MULTILINESTRING") {
+     df[["multilinestring_id"]] <-
+       rep(as.integer(factor(path[["object_"]])), ncoord)
+    df[["linestring_id"]] <-
+      rep(as.integer(factor(path[["path_"]])), ncoord)
+    df <- dplyr::arrange(df,
+                         .data$multilinestring_id,
+                         .data$linestring_id)
+  }
   # sfc_type <- sfc_type %||%  unique(path$type)
   # stopifnot(length(sfc_type) == 1L)
   # if (sfc_type == "MULTIPOLYGON") {
