f0e1a9cc8faee70018ec65b74cc1bd2c3bb4ae23
Michael Sumner <mdsumner@gmail.com>
2020-04-01 22:54:33 +1100

    to_sf

---
+++ R/000_to_sf.R
@@ -0,0 +1,43 @@
+#' Build sf from silicate
+#'
+#' Build sf
+#' @param x silicate model
+#'
+#' @return sf dataframe
+#' @export
+#' @importFrom sfbuild to_sf
+#' @export to_sf
+#' @method to_sf sc
+#' @aliases to_sf
+#' @examples
+#' ## PATHs are MULTILINESTRING, nominate LINESTRING and ignore object_
+#' to_sf(PATH(minimal_mesh))
+#' to_sf(PATH0(minimal_mesh))
+#'
+#' #to_sf(ARC(minimal_mesh))
+to_sf.sc <- function(x, sfc_type = NULL, geom_type = NULL) {
+  df <- sc_coord(x)
+  names(df) <- gsub("_", "", names(df))
+  path <- sc_path(x)
+  ncoord <- path[["ncoords_"]]
+
+  sfc_type <- "MULTILINESTRING"
+  if (sfc_type == "MULTILINESTRING") {
+    df[["multilinestring_id"]] <-
+      rep(as.integer(factor(path[["object_"]])), ncoord)
+     df[["linestring_id"]] <-
+      rep(as.integer(factor(path[["path_"]])), ncoord)
+  }
+
+  # sfc_type <- sfc_type %||%  unique(path$type)
+  # stopifnot(length(sfc_type) == 1L)
+  # if (sfc_type == "MULTIPOLYGON") {
+  #   df[["multipolygon_id"]] <-
+  #     rep(as.integer(factor(path[["object_"]])), ncoord)
+  #   df[["polygon_id"]] <-
+  #     rep(as.integer(factor(path[["subobject"]])), ncoord)
+  #   df[["linestring_id"]] <-
+  #     rep(as.integer(factor(path[["path_"]])), ncoord)
+  # }
+  sfbuild::to_sf(df)
+}
